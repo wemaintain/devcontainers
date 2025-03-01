64f96533d2b1fa69570358fd26ee25b9c88dc9c7
Farzad Senart <farzad.senart@gmail.com>
2025-02-28 19:17:22 -0500

    fix: allow multiple Node versions to co-exist (#44)
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

--- features/src/node23/install.sh
+++ features/src/node23/install.sh
@@ -76,7 +76,7 @@ npm install -g "npm@$NPM_VERSION"
 
 mkdir -p /etc/bash_completion.d
 
-cat <<EOF >>/etc/bash_completion.d/npm
+cat <<EOF >>/etc/bash_completion.d/node23-npm
 eval "\$(npm completion)"
 EOF
 
