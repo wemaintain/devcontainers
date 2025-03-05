e86aed43e352cdad32067cd826401ea02dcc130a
lion3ls <801084+lion3ls@users.noreply.github.com>
2025-03-05 12:09:40 +0100

    feature/add-nx (#2)
    Signed-off-by: Lionel Suss <801084+lion3ls@users.noreply.github.com>

---
+++ features/src/nx/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eux
+
+if [ "$UID" -ne 0 ]; then
+  echo -e "(!) User must be root: $UID"
+  exit 1
+fi
+
+ARCH="$(dpkg --print-architecture)"
+if [ "$ARCH" != 'amd64' ] && [ "$ARCH" != 'arm64' ]; then
+  echo "(!) Unsupported architecture: $ARCH"
+  exit 1
+fi
+
+#---
+
+#? https://github.com/nrwl/nx/tags
+npm install -g nx@20.4.6
