ed8ed4ec9fbd5a14fefba2d5e497aac185a205a4
Farzad Senart <farzad.senart@gmail.com>
2025-03-16 19:58:44 -0400

    feature/python313: extract deps (#59)
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

--- features/src/python313/install.sh
+++ features/src/python313/install.sh
@@ -2,28 +2,15 @@
 
 set -eux
 
-if [ "$UID" -ne 0 ]; then
-  echo -e "(!) User must be root: $UID"
-  exit 1
-fi
+# shellcheck source=../../lib/install.sh
+source dev-container-features-install-lib
 
-ARCH="$(dpkg --print-architecture)"
-if [ "$ARCH" != 'amd64' ] && [ "$ARCH" != 'arm64' ]; then
-  echo "(!) Unsupported architecture: $ARCH"
-  exit 1
-fi
-
-#---
+#? https://github.com/docker-library/python/blob/master/3.13/bookworm/Dockerfile
 
 export LANG="C.UTF-8"
-
-apt update --quiet
-apt install --yes --no-install-recommends \
+dc_install \
   build-essential \
-  ca-certificates \
-  curl \
   pkg-config \
-  xz-utils \
   \
   libbz2-dev \
   libffi-dev \
@@ -35,33 +22,20 @@ apt install --yes --no-install-recommends \
   libssl-dev \
   uuid-dev \
   zlib1g-dev
-rm -rf /var/lib/apt/lists/*
-
-#---
-
-#? https://github.com/docker-library/python/blob/master/3.13/bookworm/Dockerfile
-#? https://github.com/python/cpython/tags
-PACKAGE_VERSION=3.13.2
-PACKAGE_URL="https://www.python.org/ftp/python/${PACKAGE_VERSION}/Python-${PACKAGE_VERSION}.tar.xz"
-PACKAGE_SUM=d984bcc57cd67caab26f7def42e523b1c015bbc5dc07836cf4f0b63fa159eb56
 
 PACKAGE=/tmp/package.tar.xz
-curl -fLsS "$PACKAGE_URL" -o $PACKAGE
-echo "$PACKAGE_SUM $PACKAGE" | sha256sum -c
+dc_download python $PACKAGE
 
-BUILD_DIR=/tmp/python313
-mkdir -p $BUILD_DIR
-tar -xJf $PACKAGE --strip-components 1 -C $BUILD_DIR
-rm -f $PACKAGE
+BUILD_DIR=$(dc_mkdir /tmp/python313)
+tar -xJf $PACKAGE --strip-components 1 -C "$BUILD_DIR"
 
-INSTALL_DIR=/opt/python313
-mkdir -p $INSTALL_DIR
+INSTALL_DIR=$(dc_mkdir /opt/python313)
 
 #? https://docs.python.org/3/using/configure.html
-cd $BUILD_DIR
+cd "$BUILD_DIR"
 ./configure \
   --build="$(dpkg-architecture --query DEB_BUILD_GNU_TYPE)" \
-  --prefix=$INSTALL_DIR \
+  --prefix="$INSTALL_DIR" \
   --enable-loadable-sqlite-extensions \
   --enable-optimizations \
   --enable-option-checking=fatal \
@@ -69,8 +43,8 @@ cd $BUILD_DIR
   --with-ensurepip \
   --with-lto
 
-EXTRA_CFLAGS="$(dpkg-buildflags --get CFLAGS)"
-LDFLAGS="$(dpkg-buildflags --get LDFLAGS)"
+EXTRA_CFLAGS=$(dpkg-buildflags --get CFLAGS)
+LDFLAGS=$(dpkg-buildflags --get LDFLAGS)
 
 #? https://docs.python.org/3.12/howto/perf_profiling.html
 #? https://github.com/docker-library/python/pull/1000#issuecomment-2597021615
@@ -91,14 +65,14 @@ make install
 
 #? Enable GDB to load debugging data
 #? https://github.com/docker-library/python/pull/701
-bin=$(readlink -ve $INSTALL_DIR/bin/python3)
+bin=$(readlink -ve "$INSTALL_DIR/bin/python3")
 dir=$(dirname "$bin")
 mkdir -p "/usr/share/gdb/auto-load/$dir"
 cp -vL Tools/gdb/libpython.py "/usr/share/gdb/auto-load/$bin-gdb.py"
 
-cd /
-rm -rf $BUILD_DIR
-find $INSTALL_DIR -depth \
+cd -
+rm -rf "$BUILD_DIR"
+find "$INSTALL_DIR" -depth \
   \( \
   \( -type d -a \( -name test -o -name tests -o -name idle_test \) \) \
   -o \( -type f -a \( -name '*.pyc' -o -name '*.pyo' -o -name 'libpython*.a' \) \) \
@@ -106,20 +80,10 @@ find $INSTALL_DIR -depth \
 
 ldconfig
 
-#---
+python3 -m pip install --upgrade \
+  "build==$(dc_version build)" \
+  "pip==$(dc_version pip)" \
+  "setuptools==$(dc_version setuptools)" \
+  "wheel==$(dc_version wheel)"
 
-#? https://github.com/pypa/build/tags
-PACKAGE_VERSION=1.2.2.post1
-python3 -m pip install --upgrade build==$PACKAGE_VERSION
-
-#? https://github.com/pypa/pip/tags
-PACKAGE_VERSION=25.0.1
-python3 -m pip install --upgrade pip==$PACKAGE_VERSION
-
-#? https://github.com/pypa/setuptools/tags
-PACKAGE_VERSION=75.8.2
-python3 -m pip install --upgrade setuptools==$PACKAGE_VERSION
-
-#? https://github.com/pypa/wheel/tags
-PACKAGE_VERSION=0.45.1
-python3 -m pip install --upgrade wheel==$PACKAGE_VERSION
+rm -f /tmp/package*
