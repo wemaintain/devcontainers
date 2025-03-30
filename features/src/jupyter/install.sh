acfff188772274cd2e7b8249ba86893b738687c9
Farzad Senart <farzad.senart@gmail.com>
2025-03-30 17:20:06 -0400

    feature/jupyter: extract deps (#66)
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

--- features/src/jupyter/install.sh
+++ features/src/jupyter/install.sh
@@ -2,27 +2,13 @@
 
 set -eux
 
-if [ "$UID" -ne 0 ]; then
-  echo -e "(!) User must be root: $UID"
-  exit 1
-fi
-
-ARCH="$(dpkg --print-architecture)"
-if [ "$ARCH" != 'amd64' ] && [ "$ARCH" != 'arm64' ]; then
-  echo "(!) Unsupported architecture: $ARCH"
-  exit 1
-fi
-
-#---
+# shellcheck source=../../lib/install.sh
+source dev-container-features-install-lib
 
 if command -v python3 >/dev/null 2>&1; then
-  #? https://github.com/ipython/ipykernel/tags
-  #? https://github.com/matplotlib/ipympl/tags
-  #? https://github.com/matplotlib/matplotlib/tags
-  #? https://github.com/numpy/numpy/tags
   python3 -m pip install --upgrade \
-    "ipykernel==6.29.5" \
-    "ipympl==0.9.7" \
-    "matplotlib==3.10.1" \
-    "numpy==2.2.3"
+    "ipykernel==$(dc_version ipykernel)" \
+    "ipympl==$(dc_version ipympl)" \
+    "matplotlib==$(dc_version matplotlib)" \
+    "numpy==$(dc_version numpy)"
 fi
