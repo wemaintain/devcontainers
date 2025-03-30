0b333330a23b36cdca163163dec32c25d30b48d4
Farzad Senart <farzad.senart@gmail.com>
2025-03-30 17:33:44 -0400

    feature/node23: extract deps (#68)
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

--- features/src/node23/install.sh
+++ features/src/node23/install.sh
@@ -2,64 +2,20 @@
 
 set -eux
 
-NPM_VERSION=${NPM_VERSION:-undefined}
-
-#---
-
-if [ "$UID" -ne 0 ]; then
-  echo -e "(!) User must be root: $UID"
-  exit 1
-fi
-
-ARCH="$(dpkg --print-architecture)"
-if [ "$ARCH" != 'amd64' ] && [ "$ARCH" != 'arm64' ]; then
-  echo "(!) Unsupported architecture: $ARCH"
-  exit 1
-fi
-
-#---
-
-apt update --quiet
-apt install --yes --no-install-recommends \
-  ca-certificates \
-  curl \
-  xz-utils
-rm -rf /var/lib/apt/lists/*
-
-#---
-
-#? https://github.com/nodejs/node/tags
-PACKAGE_VERSION=23.9.0
-case $ARCH in
-amd64)
-  PACKAGE_URL="https://nodejs.org/dist/v${PACKAGE_VERSION}/node-v${PACKAGE_VERSION}-linux-x64.tar.xz"
-  PACKAGE_SUM=0b4ece2aa678e6891b9abf6118d5393867ab07b3e31c8d8c4c34e97840fa8749
-  ;;
-arm64)
-  PACKAGE_URL="https://nodejs.org/dist/v${PACKAGE_VERSION}/node-v${PACKAGE_VERSION}-linux-arm64.tar.xz"
-  PACKAGE_SUM=dc0d93c5e4ae41c8fe75b64399c4d1fe3c15e2bfa3f55f92f8697bb16397585b
-  ;;
-esac
+# shellcheck source=../../lib/install.sh
+source dev-container-features-install-lib
 
 PACKAGE=/tmp/package.tar.xz
-curl -fLsS "$PACKAGE_URL" -o $PACKAGE
-echo "$PACKAGE_SUM $PACKAGE" | sha256sum -c
+dc_download node $PACKAGE
 
-INSTALL_DIR=/opt/node23
-mkdir -p $INSTALL_DIR
-tar -xJf $PACKAGE --strip-components 1 -C $INSTALL_DIR
-rm -f $PACKAGE
+INSTALL_DIR=$(dc_mkdir /opt/node23)
+tar -xJf $PACKAGE --strip-components 1 -C "$INSTALL_DIR"
 
-#---
+npm install -g "npm@$(dc_version npm)"
 
-#? https://github.com/npm/cli/tags
-PACKAGE_VERSION=11.1.0
-npm install -g npm@$PACKAGE_VERSION
-
-#---
-
-mkdir -p /etc/bash_completion.d
-
-cat <<EOF >>/etc/bash_completion.d/node23-npm
-eval "\$(npm completion)"
+# shellcheck disable=SC2119
+dc_bash_complete node23-npm <<EOF
+eval "\$($INSTALL_DIR/bin/npm completion)"
 EOF
+
+rm -f /tmp/package*
