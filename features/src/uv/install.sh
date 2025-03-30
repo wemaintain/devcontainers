37d1814f77f6d2d7a132e2d0bd6a55c5a404d605
Farzad Senart <farzad.senart@gmail.com>
2025-03-30 18:14:35 -0400

    feature/uv: extract deps (#72)
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

--- features/src/uv/install.sh
+++ features/src/uv/install.sh
@@ -2,57 +2,21 @@
 
 set -eux
 
-if [ "$UID" -ne 0 ]; then
-  echo -e "(!) User must be root: $UID"
-  exit 1
-fi
-
-ARCH="$(dpkg --print-architecture)"
-if [ "$ARCH" != 'amd64' ] && [ "$ARCH" != 'arm64' ]; then
-  echo "(!) Unsupported architecture: $ARCH"
-  exit 1
-fi
-
-#---
-
-apt update --quiet
-apt install --yes --no-install-recommends \
-  ca-certificates \
-  curl
-rm -rf /var/lib/apt/lists/*
-
-#---
-
-#? https://github.com/astral-sh/uv/tags
-PACKAGE_VERSION=0.6.3
-case $ARCH in
-amd64)
-  PACKAGE_URL="https://github.com/astral-sh/uv/releases/download/${PACKAGE_VERSION}/uv-x86_64-unknown-linux-gnu.tar.gz"
-  PACKAGE_SUM=b7a37a33d62cb7672716c695226450231e8c02a8eb2b468fa61cd28a8f86eab2
-  ;;
-arm64)
-  PACKAGE_URL="https://github.com/astral-sh/uv/releases/download/${PACKAGE_VERSION}/uv-aarch64-unknown-linux-gnu.tar.gz"
-  PACKAGE_SUM=447726788204106ffd8ecc59396fccc75fae7aca998555265b5ea6950b00160c
-  ;;
-esac
+# shellcheck source=../../lib/install.sh
+source dev-container-features-install-lib
 
 PACKAGE=/tmp/package.tar.gz
-curl -fLsS "$PACKAGE_URL" -o $PACKAGE
-echo "$PACKAGE_SUM $PACKAGE" | sha256sum -c
+dc_download uv $PACKAGE
 
-INSTALL_DIR=/opt/bin
-mkdir -p $INSTALL_DIR
-tar -xzf $PACKAGE --strip-components 1 -C $INSTALL_DIR
-rm -f $PACKAGE
+INSTALL_DIR=$(dc_mkdir /opt/bin)
+tar -xzf $PACKAGE --strip-components 1 -C "$INSTALL_DIR"
 
-#---
-
-mkdir -p /etc/bash_completion.d
-
-cat <<EOF >>/etc/bash_completion.d/uv
+dc_bash_complete uv <<EOF
 eval "\$(uv generate-shell-completion bash)"
 EOF
 
-cat <<EOF >>/etc/bash_completion.d/uvx
+dc_bash_complete uvx <<EOF
 eval "\$(uvx --generate-shell-completion bash)"
 EOF
+
+rm -f /tmp/package*
