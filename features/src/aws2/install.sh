f169c48f1bc4235991c845549ac74b70424a2fa1
Farzad Senart <farzad.senart@gmail.com>
2025-03-30 18:29:08 -0400

    feature/aws2: use bash completion helper (#73)
    Signed-off-by: Farzad Senart <farzad.senart@gmail.com>

--- features/src/aws2/install.sh
+++ features/src/aws2/install.sh
@@ -10,17 +10,13 @@ dc_download cli $PACKAGE
 
 BUILD_DIR=/tmp/package
 unzip -q $PACKAGE -d $BUILD_DIR
-rm -f $PACKAGE
 
 BIN_DIR=/opt/bin
 INSTALL_DIR=/opt/aws2
 $BUILD_DIR/aws/install -i $INSTALL_DIR -b $BIN_DIR
-rm -rf $BUILD_DIR
 
-#---
-
-mkdir -p /etc/bash_completion.d
-
-cat <<EOF >>/etc/bash_completion.d/aws2
+dc_bash_complete aws2 <<EOF
 complete -C '$BIN_DIR/aws_completer' aws
 EOF
+
+rm -rf /tmp/package*
