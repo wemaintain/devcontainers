f0ca43e9021301d2752a5df51a154f5928d103dc
Lionel Suss <801084+lion3ls@users.noreply.github.com>
2025-04-25 16:40:04 +0000

    change token's secret name
    Signed-off-by: Lionel Suss <801084+lion3ls@users.noreply.github.com>

--- features/src/terraform/install.sh
+++ features/src/terraform/install.sh
@@ -5,13 +5,15 @@ set -eux
 # shellcheck source=../../lib/install.sh
 source dev-container-features-install-lib
 
+dc_install \
+  unzip
+
 PACKAGE=/tmp/package.zip
-dc_download cli $PACKAGE
+dc_download terraform $PACKAGE
 
 INSTALL_DIR=$(dc_mkdir /opt/bin)
 mkdir -p "$INSTALL_DIR"
 unzip $PACKAGE -d "$INSTALL_DIR"
-rm -f $PACKAGE
 
 #---
 
