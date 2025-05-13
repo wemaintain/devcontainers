df7c502b735b78b1356468f15da524baa5197aa5
Lionel Suss <801084+lion3ls@users.noreply.github.com>
2025-05-13 15:59:27 +0000

    auto-update cdktf
    Signed-off-by: Lionel Suss <801084+lion3ls@users.noreply.github.com>

--- features/src/cdktf/install.sh
+++ features/src/cdktf/install.sh
@@ -2,18 +2,8 @@
 
 set -eux
 
-if [ "$UID" -ne 0 ]; then
-  echo -e "(!) User must be root: $UID"
-  exit 1
-fi
+# shellcheck source=../../lib/install.sh
+source dev-container-features-install-lib
 
-ARCH="$(dpkg --print-architecture)"
-if [ "$ARCH" != 'amd64' ] && [ "$ARCH" != 'arm64' ]; then
-  echo "(!) Unsupported architecture: $ARCH"
-  exit 1
-fi
-
-#---
-
-#? https://www.npmjs.com/package/cdktf-cli?activeTab=versions
-npm install -g cdktf-cli@0.20.11
+npm install -g \
+  "cdktf-cli@$(dc_version ng)"
