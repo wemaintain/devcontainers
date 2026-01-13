6e3e4c79b90727b55dd175194694fbcace3da59d
Lionel Suss <801084+lion3ls@users.noreply.github.com>
2026-01-13 15:43:22 +0000

    euh ?
    Signed-off-by: Lionel Suss <801084+lion3ls@users.noreply.github.com>

--- features/src/cdktf/install.sh
+++ features/src/cdktf/install.sh
@@ -5,5 +5,8 @@ set -eux
 # shellcheck source=../../lib/install.sh
 source dev-container-features-install-lib
 
+# Install dependencies for building native modules
+dc_install python3 make g++
+
 npm install -g \
   "cdktf-cli@$(dc_version ng)"
